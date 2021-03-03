c040ce9658981e56d97231d8e5d8b424eedd2e46
Bholanath Barik <71944044+bholanathbarik9748@users.noreply.github.com>
2021-03-04 01:08:28 +0530

    crete database set and set table ,set guast button and drowable button and improve docs

--- the_pizza_shop/settings.py
+++ the_pizza_shop/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'the_pizza_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'The Pizza Shop',
+        'USER': 'postgres',
+        'PASSWORD': 'messi@2002',
+        'HOST': 'localhost',
     }
 }
 
